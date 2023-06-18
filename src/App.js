c1dd260c578a549825650bfe6685839c34086651
James Chen <78395974+invacctvni@users.noreply.github.com>
2023-06-17 21:28:00 -0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,43 @@
 import logo from './logo.svg';
 import './App.css';
-
+import {useEffect, useState} from "react"
+import axios from "axios"
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  const [productList, setProductList] = useState([])
+  const fetchData = async () => {
+    try {
+      let res = await axios.get("http://localhost:3000/getProductAll",{
+        timeout: 30000
+      })
+      if (res.data.rs) {
+        console.log(res.data.data)
+        setProductList(res.data.data)
+      }
+      // console.log(res);
+    } catch(e) {
+      console.log(e)
+    }
+  }
+
+  useEffect( () => fetchData,[])
+  return (<table>
+    <tbody>
+    <tr>
+      <th>Product</th>
+      <th>Product Name</th>
+      <th>Slug</th>
+      <th>Description</th>
+      <th>Price</th>
+    </tr>
+    {productList.map((v) => <tr key={v?._id}>
+      <td>{v?._id}</td>
+      <td>{v?.name}</td>
+      <td>{v?.slug}</td>
+      <td>{v?.description}</td>
+      <td>{v?.price}</td>
+    </tr>)}
+    </tbody>
+    </table>
   );
 }
 
